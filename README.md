2776b6766fb5da22041d6c2243c136b755ad3317
VeselinGerenski <veselingerenski@gmail.com>
2025-10-15 16:49:57 +0300

    Add Express Server

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # movie-magic-sept2025
 Softuni JS Back-End Course
+
+## Development Steps
+
+- [x] Initialize Project
+- [] Add Express Server
+- [] Add workshop resources
+- [] Add Handlebars
+- [] Render Home Page
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("it Works!");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req,res) =>{
+   res.send('It works');
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000..'));
