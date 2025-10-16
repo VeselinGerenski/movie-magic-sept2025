00d396457fc09c0847ecc1f6c2c83588df8f7a6e
VeselinGerenski <veselingerenski@gmail.com>
2025-10-16 14:47:04 +0300

    Add movie data layer

---
+++ src/models/Movie.js
@@ -0,0 +1,43 @@
+const movies = [
+    {
+        id: 'a3582672-0ee4-1284-8759-35ee253329zv',
+        title: 'Jungle Cuise',
+        genre: 'Adventure',
+        description: 'dwadwadwadawdwadawda',
+        imageUrl: '\img\jungle-cruise.jpeg',
+        director: 'Jaume Collet - Serra',
+        year: '2021',
+        rating: '6.6',
+        category: 'movie',
+    },
+     {
+        id: 'z2682672-0ee4-1534-8759-35ee253329ty',
+        title: 'Man on Steel',
+        genre: 'Superhero',
+        description: 'An alien...',
+        imageUrl: 'https://www.usatoday.com/gcdn/media/USATODAY/GenericImages/2013/06/12/1371055927000-AP-FILM-REVIEW-MAN-OF-STEEL-56426827-1306121255_3_4.JPG',
+        director: 'Zack Snyder',
+        year: '2013',
+        rating: '7.1',
+        category: 'movie',
+    }, 
+    {
+        id: 'a3554372-0ee4-1284-8759-35ee253329zv',
+        title: 'Home Alone',
+        genre: 'Comedy',
+        description: 'dwadwadwadawdwadawda',
+        imageUrl: '\img\home-alone.jpeg',
+        director: 'dwadwadaw',
+        year: '2002',
+        rating: '9.6',
+        category: 'movie',
+    },
+
+]
+
+class Movie {
+    static find() {
+      
+        return movies.slice();
+    }
+}
