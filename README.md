6d7fba225209afa5760c4aebfd63f26ed189daa4
VeselinGerenski <veselingerenski@gmail.com>
2025-10-17 15:41:18 +0300

    Add unique id for each created movie

--- README.md
+++ README.md
@@ -34,8 +34,6 @@ Softuni JS Back-End Course
 
 
 
-  
-
 
 
 
--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -10,7 +10,9 @@ movieController.get('/create', (req, res) => {
 movieController.post('/create', (req, res) => {
    const movieData = req.body;
 
-    movieService.create(movieData);
+  const movie = movieService.create(movieData);
+
+  movie.id;
 
     res.redirect('/');
 });
